cb62afc41fd34b1bd1f2a1d90b93094d69129c07
xi-forgetme <xiforgetme@yahoo.com>
2023-05-10 20:57:12 +0800

    Initial commit

---
+++ src/main/java/us/wanderfly/wanderin/WanderinApplication.java
@@ -0,0 +1,13 @@
+package us.wanderfly.wanderin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WanderinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WanderinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/us/wanderfly/wanderin/WanderinApplicationTests.java
@@ -0,0 +1,13 @@
+package us.wanderfly.wanderin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WanderinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
